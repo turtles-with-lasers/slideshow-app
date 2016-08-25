67d1cd2f4737493d6a8e413c95543ba13ea9eb6c
mkamranhamid <mkamranhamid@gmail.com>
2016-08-25 14:25:28 +0500

    implemented home screen

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,7 @@
 app.controller('homeCtrl',function($scope){
     $scope.name = 'KAKA';
+    $scope.addUser = false;
+    $scope.openAddUser = function(){
+        $scope.addUser = true;
+    };
 })
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,7 @@
-app.controller('loginCtrl',function($scope){
+app.controller('loginCtrl',function($scope, $state){
     $scope.name = 'KAKA';
-})
+    $scope.login = function (user) {
+        console.log(user);
+        $state.go('home');
+    }
+});
